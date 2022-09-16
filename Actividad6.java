a78cf6bdbe2880d1e968719bc422a9e29dab9c42
Rodrigo Ortega <rodrigoortega@ciencias.unam.mx>
2022-09-16 16:19:12 -0500

    Creación de clases Actividad6 y SimpleLinkedList

---
+++ Actividad6.java
@@ -0,0 +1,70 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Random;
+
+public class Actividad6 {
+
+    public static int arrayEncuentraValorMitad(ArrayList<String> arrayList, int b){
+        int mitad = (arrayList.size()/2) + 1;
+        
+        if (b < 0 || b > arrayList.size() || b > mitad || !arrayList.contains(arrayList.get(b))){
+            arrayList.get(b);
+            return -1;
+        } else {
+            return 1;
+        }
+    }
+        
+
+
+    public static void main(String[] args){
+        long inicio;
+        long fin;
+        
+        Random random = new Random();
+        SimpleLinkedList<String> listaSimple = new SimpleLinkedList<>();
+        DoubleLinkedList<String> listaDoble = new DoubleLinkedList<>();
+        ArrayList<String> arrayList = new ArrayList<>();
+
+        int n = 0;
+        while(n < 100000){
+            listaDoble.add(listaDoble.size(), (random.ints(1000, 10, 1000).toString()));
+            listaSimple.add(listaSimple.size(), (random.ints(1000, 10, 1000).toString()));
+            arrayList.add(random.ints(1000, 10, 1000).toString());    
+            n++;
+        }
+        
+        System.out.println(listaDoble.size());
+        System.out.println(listaSimple.size());
+        System.out.println(arrayList.size());
+
+        // inserción al final de 1000 elementos 
+        inicio = System.currentTimeMillis();
+        //listaSimple.add(random.nextInt(listaSimple.size()), "a");
+        //listaSimple.clear();
+        //listaSimple.get(random.nextInt(listaSimple.size()));
+        listaSimple.encuentraValorMitad(5000);
+        fin = System.currentTimeMillis();
+        System.out.println("Lista simplemente ligada: " + (fin - inicio) + "ms.");
+        
+        inicio = System.currentTimeMillis();
+        //listaDoble.add(random.nextInt(listaDoble.size()), "a");
+        //listaDoble.revert();
+        //listaSimple.clear();
+        //listaDoble.get(random.nextInt(listaDoble.size()));
+        listaDoble.encuentraValorMitad(5000);
+        fin = System.currentTimeMillis();
+        System.out.println("Lista doblemente ligada: " + (fin - inicio) + "ms.");
+
+        inicio = System.currentTimeMillis();
+        //Collections.reverse(arrayList);
+        //arrayList.clear();
+        //arrayList.get(random.nextInt(arrayList.size()));
+        arrayEncuentraValorMitad(arrayList, 200);
+        //fin = System.currentTimeMillis();
+        System.out.println("Array list: " + (fin - inicio) + "ms.");
+
+        
+
+    }
+}
